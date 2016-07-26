e7557c1d3bca0e774aa4a6bf5773156da6cb2de7
Dong Liu <doliu@microsoft.com>
2016-07-26 15:13:27 +0800

    Add history function.

---
+++ src/haterm.lib/Hastory.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace haterm
+{
+    public class SearchCombo
+    {
+        public static SearchCombo NotFound = new SearchCombo();
+
+        public string Line { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class Hastory
+    {
+        private const string defaultPrefix = "ha";
+        private List<string> list = new List<string>();
+        private string filepath;
+
+        public Hastory(Guid id, string filePrefix = defaultPrefix)
+        {
+            var home = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%");
+            Directory.CreateDirectory($"{home}\\haterm");
+            filepath = $"{home}\\haterm\\{filePrefix}-{id}";
+        }
+
+        public void Load()
+        {
+            try
+            {
+                list = File.ReadAllLines(filepath).ToList();
+            }
+            catch (FileNotFoundException)
+            {
+            }
+        }
+
+        public void Add(string line)
+        {
+            list.Add(line);
+        }
+
+        public void Save()
+        {
+            File.WriteAllLines(filepath, list);
+        }
+
+        public int Count => list.Count;
+
+        public SearchCombo Search(string prefix, int id = -1, bool backward = true)
+        {
+            if (id == -1)
+            {
+                id = this.list.Count;
+            }
+
+            if (backward)
+            {
+                for (var i = id - 1; i >= 0; i--)
+                {
+                    if (list[i].StartsWith(prefix))
+                    {
+                        return new SearchCombo()
+                        {
+                            Line = list[i],
+                            Id = i
+                        };
+                    }
+                }
+            }
+            else
+            {
+                for (var i = id + 1; i < list.Count; i++)
+                {
+                    if (list[i].StartsWith(prefix))
+                    {
+                        return new SearchCombo()
+                        {
+                            Line = list[i],
+                            Id = i
+                        };
+                    }
+                }
+            }
+
+            return SearchCombo.NotFound;
+        }
+    }
+}
---
+++ test/haterm.test/HastoryTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace haterm.test
+{
+    [TestClass]
+    public class HastoryTest
+    {
+        [TestMethod]
+        public void AddGetHistory()
+        {
+            var guid = Guid.NewGuid();
+            var his = new Hastory(guid, "test");
+            his.Load();
+            his.Count.Should().Be(0);
+            his.Add("ab123");
+            his.Count.Should().Be(1);
+
+            var his2 = new Hastory(Guid.NewGuid(), "test");
+            his2.Load();
+            his2.Count.Should().Be(0);
+
+            var his3 = new Hastory(guid, "test");
+            his3.Load();
+            his3.Count.Should().Be(0);
+
+            his.Save();
+            his3.Load();
+            his3.Count.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void SearchHistory()
+        {
+            var guid = Guid.NewGuid();
+            var his = new Hastory(guid, "test");
+            his.Load();
+            his.Count.Should().Be(0);
+            his.Add("ab123");
+            his.Add("bc123");
+
+            var combo = his.Search("a");
+            combo.Line.Should().Be("ab123");
+            combo.Id.Should().Be(0);
+
+            var combo1 = his.Search("");
+            combo1.Line.Should().Be("bc123");
+            combo1.Id.Should().Be(1);
+
+            var combo2 = his.Search("56");
+            combo2.Should().Be(SearchCombo.NotFound);
+        }
+
+        [TestMethod]
+        public void SearchHistoryWithId()
+        {
+            var guid = Guid.NewGuid();
+            var his = new Hastory(guid, "test");
+            his.Load();
+            his.Count.Should().Be(0);
+            his.Add("at00");
+            his.Add("bt01");
+            his.Add("at02");
+            his.Add("bt03");
+            his.Add("bt04");
+            his.Add("at05");
+
+            var combo = his.Search("a");
+            combo.Line.Should().Be("at05");
+            combo.Id.Should().Be(5);
+
+            combo = his.Search("a", 5);
+            combo.Line.Should().Be("at02");
+            combo.Id.Should().Be(2);
+
+            combo = his.Search("a", 2);
+            combo.Line.Should().Be("at00");
+            combo.Id.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void SearchHistoryWithIdForward()
+        {
+            var guid = Guid.NewGuid();
+            var his = new Hastory(guid, "test");
+            his.Load();
+            his.Count.Should().Be(0);
+            his.Add("at00");
+            his.Add("bt01");
+            his.Add("at02");
+            his.Add("bt03");
+            his.Add("bt04");
+            his.Add("at05");
+
+            var combo = his.Search("a");
+            combo.Line.Should().Be("at05");
+            combo.Id.Should().Be(5);
+
+            combo = his.Search("a", 5, false);
+            combo.Should().Be(SearchCombo.NotFound);
+
+            combo = his.Search("b", 1, false);
+            combo.Line.Should().Be("bt03");
+            combo.Id.Should().Be(3);
+        }
+    }
+}
