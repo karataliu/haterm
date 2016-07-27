18fedcf687116770514a37c629130e7256d6561a
Dong Liu <doliu@microsoft.com>
2016-07-27 12:34:32 +0800

    Add netfx project.

---
+++ haterm.netfx/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace haterm.netfx
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var shell = new CmdShell(new DualWrapper()))
+            {
+                var mc =new Haterm(CmdTerminal.Instance, shell);
+                mc.Run();
+            }
+        }
+    }
+}
